f77d7a4419231d4a62fb50e34a6872ddf93d8242
Aman kumar <45587567+aman-ku@users.noreply.github.com>
2020-01-11 11:27:28 +0530

    Just added object type

--- LinkedListDriver.java
+++ LinkedListDriver.java
@@ -3,12 +3,13 @@ interface List<T>
     public void add(T a);
     public void delete(T b);
     public void traverse();
-    // public void search(T c);
+    public int search(T c);
+    public void add(T a,int p);
 }
 class Node<T>
 {
     T val;
-    Node next;
+    Node<T> next;
     Node(T v)
     {
         val=v;
@@ -26,7 +27,7 @@ class LinkedList<T> implements List<T>
     }
     public void add(T v)
     {
-        Node ob=new Node(v);
+        Node<T> ob=new Node<T>(v);
         if(head==null)
         {
             head=tail=ob;
@@ -41,8 +42,8 @@ class LinkedList<T> implements List<T>
     }
     public void delete(T v)
     {
-        Node d=head;
-        Node f=head;
+        Node<T> d=head;
+        Node<T> f=head;
         while(f.next!=null)
         {
             if(d.val==v && head.val==v)
@@ -78,7 +79,7 @@ class LinkedList<T> implements List<T>
     }
     public void traverse()
     {
-        Node a=head;
+        Node<T> a=head;
         while(a.next!=null)
         {
             System.out.println(a.val);
@@ -86,6 +87,39 @@ class LinkedList<T> implements List<T>
         }
         System.out.println(a.val);
 
+    }
+    public int search(T c)
+    {
+        Node<T> ob=head;
+        int i=0;
+        while(ob.next!=null)
+        {
+            if(ob.val!=c)
+            {
+                i++;
+                ob=ob.next;
+            }
+            else{
+                break;
+            }
+        }
+        return i;
+    }
+    public void add(T a,int p)
+    {
+        Node<T> key=new Node<T>(a);
+        int i=0;
+        Node<T> ob=head;
+        Node<T> ob1=head.next;
+        while(i<p-1)
+        {
+            ob=ob.next;
+            ob1=ob1.next;
+            i++;
+        }
+        ob.next=key;
+        ob=ob.next;
+        ob.next=ob1;
     }
 }
 class LinkedListDriver
@@ -97,8 +131,11 @@ class LinkedListDriver
         l.add(2);
         l.add(3);
         l.add(8);
-        l.delete(8);
+        l.add(10,2);
+        // l.delete(8);
         l.traverse();
+        int k=l.search(3);
+        System.out.println(k);
     }
 
 }
